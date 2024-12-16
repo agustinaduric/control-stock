7674120c6f2105e206ef7a9a50ac97b21f01acb0
agustinaduric <130220670+agustinaduric@users.noreply.github.com>
2024-12-16 16:59:42 -0300

    Mejora menu

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
-from src import funciones
+from src import opciones_menu
+from src import menu
 
 productos = []
 
 while True:
-    print("\nMenu de gestion de stock")
-    print("\t1. Agregar producto")
-    print("\t2. Consultar datos de un producto")
-    print("\t3. Modificar cantidad de stock")
-    print("\t4. Mostrar todos los productos")
-    print("\t5. Salir")
+    menu.menu()
 
-    opcion = int(input("\nSelecione una opcion: "))
+    opcion = int(input("Selecione una opcion: "))
 
     if opcion == 1:
-        funciones.agregar_producto(productos)
+        opciones_menu.registrar_producto(productos)
     elif opcion == 2:
-        print()
+        opciones_menu.actualizar_producto(productos)
     elif opcion == 3:
-        print()
+        opciones_menu.eliminar_producto(productos)
     elif opcion == 4:
-        if len(productos) == 0:
-            print("\nNo hay productos cargados en el sistema")
-        else:
-            for producto in productos:
-                print(f"\nProducto: {producto['nombre']}\t Precio: {producto['precio']}\t Stock: {producto['cantidad']}")
+        opciones_menu.mostrar_productos(productos)
     elif opcion == 5:
+        opciones_menu.reporte_bajo_stock(productos)
+    elif opcion == 6:
         print("\nEl programa ha finalizado correctamente\n")
         break
     else:
