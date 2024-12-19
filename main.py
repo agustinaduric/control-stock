c79c6595b772035d518eceebc430c4da4d30f47e
agustinaduric <130220670+agustinaduric@users.noreply.github.com>
2024-12-19 20:22:34 -0300

    Menú adaptado a db

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-from src import opciones_menu
-from src import menu
-
-productos = []
+from src import opciones_menu, menu
 
 while True:
     menu.menu()
 
-    opcion = int(input("Selecione una opcion: "))
+    opcion = int(input("\nSelecione una opcion: "))
 
     if opcion == 1:
-        opciones_menu.registrar_producto(productos)
+        opciones_menu.registrar_producto()
     elif opcion == 2:
-        opciones_menu.actualizar_producto(productos)
+        opciones_menu.actualizar_producto()
     elif opcion == 3:
-        opciones_menu.eliminar_producto(productos)
+        opciones_menu.eliminar_producto()
     elif opcion == 4:
-        opciones_menu.mostrar_productos(productos)
+        opciones_menu.mostrar_productos()
     elif opcion == 5:
-        opciones_menu.reporte_bajo_stock(productos)
+        opciones_menu.reporte_bajo_stock()
     elif opcion == 6:
-        print("\nEl programa ha finalizado correctamente\n")
+        print("\nEl programa ha finalizado correctamente.\n")
         break
     else:
-        print("\nIngrese una opcion valida")
+        print("\nIngrese una opcion valida.")
