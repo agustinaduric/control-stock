f8973836baa245b1735689184e7d0743739f202b
agustinaduric <130220670+agustinaduric@users.noreply.github.com>
2024-12-20 14:43:41 -0300

    Menú principal: buscar producto

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from src import opciones_menu, menu
 
 while True:
-    menu.menu()
+    menu.menu_principal()
 
     opcion = int(input("\nSelecione una opcion: "))
 
     if opcion == 1:
         opciones_menu.registrar_producto()
     elif opcion == 2:
-        opciones_menu.actualizar_producto()
+        opciones_menu.buscar_producto_menu()
     elif opcion == 3:
-        opciones_menu.eliminar_producto()
+        opciones_menu.actualizar_producto()
     elif opcion == 4:
-        opciones_menu.mostrar_productos()
+        opciones_menu.eliminar_producto()
     elif opcion == 5:
-        opciones_menu.reporte_bajo_stock()
+        opciones_menu.mostrar_productos()
     elif opcion == 6:
+        opciones_menu.reporte_bajo_stock()
+    elif opcion == 7:
         print("\nEl programa ha finalizado correctamente.\n")
         break
     else:
